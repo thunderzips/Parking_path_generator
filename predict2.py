9ef424d4531f3e8ef11e8c83b311412e652c1515
ysk <ysk@pop-os.localdomain>
2023-08-22 15:05:41 +0530

    cleaning up

--- predict2.py
+++ predict2.py
@@ -1,4 +1,3 @@
-# example of loading a pix2pix model and using it for one-off image translation
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
@@ -7,15 +6,10 @@ from matplotlib import pyplot
 
 from time import time
 
-# load an image
 def load_image(filename, size=(256,256)):
-	# load image with the preferred size
 	pixels = load_img(filename, target_size=size)
-	# convert to numpy array
 	pixels = img_to_array(pixels)
-	# scale from [0,255] to [-1,1]
 	pixels = (pixels - 127.5) / 127.5
-	# reshape to 1 sample
 	pixels = expand_dims(pixels, 0)
 	return pixels
 
@@ -45,8 +39,6 @@ for im in range(1,150):
 
 	src_image = load_image(f'images/testing/input_images/environment{im}.png')
 	print('Loaded', src_image.shape)
-	# load model
-	# generate image from source
 
 	start_time = time()
 
